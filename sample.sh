0ff589e5a72ff263f497a3c0d4b4421ae8c5c6d9
smallyunet <dev.wangyu@gmail.com>
2025-07-31 12:21:09 +0800

    update
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

---
+++ sample.sh
@@ -0,0 +1,10 @@
+python sample.py --out_dir=out-blog --device=cpu \
+                 --num_samples=3 \
+                 --max_new_tokens=200 \
+                 --temperature=0.9
+
+# python sample.py --out_dir=out-blog --device=cpu \
+#                  --num_samples=3 \
+#                  --start="区块链真正的价值在于" \
+#                  --max_new_tokens=150 \
+#                  --temperature=0.8
