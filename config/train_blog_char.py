43c7743933bc8b78594fd61bde2757c6a2883298
smallyunet <dev.wangyu@gmail.com>
2025-07-31 13:39:04 +0800

    adjust config
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- config/train_blog_char.py
+++ config/train_blog_char.py
@@ -3,38 +3,36 @@
 
 dataset  = 'dataset/blog_char'
 out_dir  = 'out-blog'
-device = 'cpu'
-compile = False
 
-eval_interval = 250 # keep frequent because we'll overfit
+eval_interval = 100
 eval_iters = 200
 log_interval = 10 # don't print too too often
 
-# we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
 wandb_log = False # override via command line if you like
 wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 32 
+block_size = 128
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+n_layer = 4
+n_head  = 4
+n_embd  = 256
+dropout = 0.3
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+learning_rate = 2e-3
+max_iters = 2000
+lr_decay_iters = 2000
+min_lr = 1e-4
+weight_decay = 0.1
+
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
-# on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+device = 'cpu'
+compile = False
